3bb41a00b2c1d23695ae8cb7a0536a8dc872add0
Martin Graversen <martin@graversen.io>
2020-05-12 20:46:08 +0200

    :construction: Basic event abstraction

---
+++ fiber-event/src/main/java/io/graversen/fiber/event/BaseEvent.java
@@ -0,0 +1,60 @@
+package io.graversen.fiber.event;
+
+import io.graversen.fiber.utils.IdUtils;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseEvent implements IEvent {
+    private final String id;
+    private final LocalDateTime eventEmittedAt;
+
+    private LocalDateTime eventPropagatedAt;
+    private LocalDateTime eventHandledAt;
+
+    protected BaseEvent(String id) {
+        this.id = id;
+        this.eventEmittedAt = LocalDateTime.now();
+    }
+
+    protected BaseEvent() {
+        this(IdUtils.fastEventId());
+    }
+
+    @Override
+    public String id() {
+        return id;
+    }
+
+    @Override
+    public LocalDateTime eventEmittedAt() {
+        return eventEmittedAt;
+    }
+
+    @Override
+    public LocalDateTime eventPropagatedAt() {
+        return eventPropagatedAt;
+    }
+
+    @Override
+    public LocalDateTime eventHandledAt() {
+        return eventHandledAt;
+    }
+
+    @Override
+    public void propagate() {
+        if (this.eventPropagatedAt == null) {
+            this.eventPropagatedAt = LocalDateTime.now();
+        } else {
+            throw new IllegalStateException(String.format("%s already propagated!", getClass().getSimpleName()));
+        }
+    }
+
+    @Override
+    public void finish() {
+        if (this.eventHandledAt == null) {
+            this.eventHandledAt = LocalDateTime.now();
+        } else {
+            throw new IllegalStateException(String.format("%s already finished!", getClass().getSimpleName()));
+        }
+    }
+}
---
+++ fiber-utils/src/main/java/io/graversen/fiber/utils/IdUtils.java
@@ -0,0 +1,15 @@
+package io.graversen.fiber.utils;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Random;
+import java.util.UUID;
+
+@UtilityClass
+public class IdUtils {
+    private final Random RANDOM = new Random();
+
+    public String fastEventId() {
+        return new UUID(RANDOM.nextLong(), RANDOM.nextLong()).toString();
+    }
+}

65cdfe887b6ecb3e2040fa798f546f8678504dd0
Martin Graversen <martin@graversen.io>
2020-05-31 17:47:33 +0200

    :fire: Clean up NetworkHooksDispatcher

--- fiber-core/src/main/java/io/graversen/fiber/core/hooks/NetworkHooksDispatcher.java
+++ fiber-core/src/main/java/io/graversen/fiber/core/hooks/NetworkHooksDispatcher.java
@@ -4,22 +4,14 @@ import io.graversen.fiber.utils.ControllableTaskLoop;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.function.Consumer;
 
 @Slf4j
 @RequiredArgsConstructor
 public class NetworkHooksDispatcher extends ControllableTaskLoop<EnqueuedNetworkHook<?>> {
     private final BlockingQueue<EnqueuedNetworkHook<?>> networkHookQueue = new LinkedBlockingQueue<>();
     private final INetworkHooks<?> networkHooks;
-
-    public <T extends BaseNetworkHook<?>> void enqueue(T networkHook) {
-        final var networkHookConsumer = this.<T>dispatchStrategies().getOrDefault(networkHook.getClass(), failureStrategy());
-        final var enqueuedNetworkHook = new EnqueuedNetworkHook<>(networkHook, networkHookConsumer);
-        networkHookQueue.offer(enqueuedNetworkHook);
-    }
 
     public void enqueue(NetworkRead<?> networkRead) {
         networkHookQueue.offer(
@@ -53,19 +45,5 @@ public class NetworkHooksDispatcher extends ControllableTaskLoop<EnqueuedNetwork
     @Override
     public EnqueuedNetworkHook<?> awaitNext() throws InterruptedException {
         return networkHookQueue.take();
-    }
-
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public <T extends BaseNetworkHook<?>> Map<Class<?>, Consumer<T>> dispatchStrategies() {
-        return Map.of(
-                NetworkRead.class, networkHook -> networkHooks.onNetworkRead((NetworkRead) networkHook),
-                NetworkWrite.class, networkHook -> networkHooks.onNetworkWrite((NetworkWrite) networkHook),
-                ClientConnected.class, networkHook -> networkHooks.onClientConnected((ClientConnected) networkHook),
-                ClientDisconnected.class, networkHook -> networkHooks.onClientDisconnected((ClientDisconnected) networkHook)
-        );
-    }
-
-    private <T extends BaseNetworkHook<?>> Consumer<T> failureStrategy() {
-        return enqueuedNetworkHook -> log.error("Could not enqueue network hook: {}", enqueuedNetworkHook);
     }
 }

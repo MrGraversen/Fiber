866b16d0d7ab1f97cdc5b24cc1ff84634de9a0a9
Martin Graversen <martin@graversen.io>
2020-06-05 20:41:36 +0200

    :sparkles: Platform query for ClientsContext

--- fiber-core/src/main/java/io/graversen/fiber/core/IPlatform.java
+++ fiber-core/src/main/java/io/graversen/fiber/core/IPlatform.java
@@ -4,8 +4,12 @@ import io.graversen.fiber.core.codec.EncodeContext;
 import io.graversen.fiber.core.codec.IDecoder;
 import io.graversen.fiber.core.codec.IEncoder;
 import io.graversen.fiber.core.codec.IReceiver;
+import io.graversen.fiber.core.tcp.ClientsContext;
+import io.graversen.fiber.core.tcp.ITcpNetworkClient;
 import io.graversen.fiber.core.tcp.ServerNetworkConfiguration;
 import io.graversen.fiber.utils.IClient;
+
+import java.util.function.Predicate;
 
 public interface IPlatform<C extends IClient> {
     void start(ServerNetworkConfiguration networkConfiguration);
@@ -17,4 +21,6 @@ public interface IPlatform<C extends IClient> {
     <T> void registerDecoder(IDecoder<T> codec, IReceiver<T> receiver);
 
     <T> EncodeContext<T> registerEncoder(IEncoder<T> encoder);
+
+    ClientsContext getClients(Predicate<ITcpNetworkClient> query);
 }
